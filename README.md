6af83e1fb9318444284368837c93e9aaec64ccfe
Alex Entwistle <alexentwistleseo@gmail.com>
2019-05-20 10:28:21 +0100

    Added readme and script

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-"# redirect-checker" 
+# Redirect-checker
+
+Use Requests library to request a URL and check it's: 
+- returned URL 
+- status code
+- redirect history
+
+This will list out the redirect history alongside a response code for each step in a redirect chain.
---
+++ redirect-trace.py
@@ -0,0 +1,23 @@
+import requests
+url = "http://www.webucator.com/index.cfm"
+
+# Request the above URL
+r = requests.get(url)
+
+# Inspect the returned status code
+# print(r.status_code)
+
+# Inspect the returned URL
+# print(r.url)
+
+# Inspect the history
+# print(r.history)
+
+# Show step-by-step history:
+r.history
+
+for i, response in enumerate(r.history, 1): 
+	print(i,response.url,response.status_code)
+
+print(r.status_code,r.url)
+
